47ffcfd268766473a073bf95a436322ea937f473
abhishek <abhishekparmarjnv@gmail.com>
2022-04-12 20:46:55 +0530

    update for hosting

---
+++ build/static/js/main.a3f7a0f6.chunk.js
@@ -0,0 +1,19 @@
+/*!
+
+=========================================================
+* Argon Dashboard PRO React - v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-pro-react
+* Copyright 2021 Creative Tim (http://www.creative-tim.com)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*! For license information please see main.a3f7a0f6.chunk.js.LICENSE.txt */
+(this["webpackJsonpargon-dashboard-pro-react"]=this["webpackJsonpargon-dashboard-pro-react"]||[]).push([[0],{23:function(e,t,c){},24:function(e,t,c){},28:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),a=c(8),o=c.n(a),s=(c(15),c(16),c(17),c(18),c(19),c(20),c(21),c(22),c(23),c(24),c(9)),i=c(31),l=c(32),d=c(33),b=c(2),j=[1,2,3,4,5,6,7,8,9],h=function(){var e=r.a.useState(null),t=Object(s.a)(e,2),c=t[0],n=t[1];return Object(b.jsx)(b.Fragment,{children:Object(b.jsx)(i.a,{className:"justify-content-center mt-5",children:Object(b.jsx)(l.a,{md:"6",children:Object(b.jsx)(i.a,{children:j.map((function(e){return Object(b.jsx)(l.a,{sm:"4",className:"my-3",children:Object(b.jsx)(d.a,{className:"p-5",style:{hieght:80,width:"100%",border:"1px solid black",borderColor:c===e?"green":"black",color:c===e?"white":"black",backgroundColor:c===e?"green":"white"},onClick:function(t){n(e)},children:e})})}))})})})})};o.a.render(Object(b.jsx)("div",{children:Object(b.jsx)(h,{})}),document.getElementById("root"))}},[[28,1,2]]]);
+//# sourceMappingURL=main.a3f7a0f6.chunk.js.map
